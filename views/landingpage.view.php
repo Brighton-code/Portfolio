2f3e39a765d52e8ab784e3b8e2fea425c6e95928
bright <Brighton@vanrouendal.nl>
2024-10-13 22:57:49 +0200

    main content

--- views/landingpage.view.php
+++ views/landingpage.view.php
@@ -22,7 +22,42 @@
 		</header>
 
 		<main>
-
+			<!-- welkom -->
+			<section class="main-border grid-row-1 grid-col-2">
+				<div class="legend">Welkom</div>
+				<p>Ik ben <strong>Brighton van Rouendal</strong> een software engineer en jong professional die zijn eerste stappen neemt in het werk leven. Kom bekijk mij portfolio en/of neem contact op met mij ☺</p>
+			</section>
+			<!-- photo -->
+			<section class="main-border grid-row-2 grid-col-2">
+				<div class="legend">Photo</div>
+				<img src="#" alt="photo Brighton van Rouendal" width="100%" height="100%">
+			</section>
+			<!-- about -->
+			<section class="about main-border grid-row-2 grid-col-2">
+				<div class="legend">Over mij</div>
+				<p>Hallo, ik ben Brighton van Rouendal een software engineer uit nederland die gepassioneerd is in het maken van project.</p>
+				<ul>
+					<p>De programmeer talen die ik ken</p>
+					<li>JavaScript</li>
+					<li>PHP</li>
+					<li>Python</li>
+				</ul>
+				<p>Ook lees ik graag boeken in mij vrijetijd, voornamelijk fantasy boeken. Soms start ik ook een een game op zoals satisfactory, factorio of minecraft.
+					Al ken ik nog niet veel talen ik ben altijd blij er een bij te leren.</p>
+			</section>
+			<!-- contact -->
+			<section class="main-border grid-row-2 grid-col-1">
+				<div class="legend">Contact</div>
+			</section>
+			<!-- blog -->
+			<section class="main-border grid-row-2 grid-col-1">
+				<div class="legend">Blog</div>
+			</section>
+			<!-- projecten -->
+			<section class="main-border grid-row-1 grid-col-2">
+				<div class="legend">Projecten</div>
+				<p>Projecten</p>
+			</section>
 		</main>
 
 		<footer>
