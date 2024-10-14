e1bf52ebe9cfa7c342a5ea182f5cbd454d577269
bright <Brighton@vanrouendal.nl>
2024-10-14 20:44:36 +0200

    verandert layout
    Weg met contact block nu in welkom tag er naar toe. Footer blok toegevoegt en ervaring block toegevoegd

--- views/landingpage.view.php
+++ views/landingpage.view.php
@@ -14,10 +14,10 @@
 			<nav id="header-nav">
 				<a href="/">Home</a>
 				<a href="/portfolio">Portfolio</a>
-				<a href="/blog">Blog</a>
 				<!-- <a href="/about">About</a> -->
-				<a href="/contact">Contact</a>
-				<a href="/login">Login</a>
+				<a href="/experience">ervaring</a>
+				<a href="/blog">Blog</a>
+				<!-- <a href="/login">Login</a> -->
 			</nav>
 		</header>
 
@@ -26,6 +26,14 @@
 			<section class="main-border grid-row-1 grid-col-2">
 				<div class="legend">Welkom</div>
 				<p>Ik ben <strong>Brighton van Rouendal</strong> een software engineer en jong professional die zijn eerste stappen neemt in het werk leven. Kom bekijk mij portfolio en/of neem contact op met mij ☺</p>
+				<section class="links">
+					<a href="#github"><img src="/public/img/github-mark.png" alt="github" height="36"></a>
+					<a href="#linkedin"><img src="/public/img/LI-In-Bug.png" alt="linkedin" height="36"></a>
+					<a href="#discord"><img src="/public/img/discord-mark-black.png" alt="discord" height="36"></a>
+					<a href="#contact">
+						<p>Contact</p>
+					</a>
+				</section>
 			</section>
 			<!-- photo -->
 			<section class="main-border grid-row-2 grid-col-2">
@@ -35,7 +43,7 @@
 			<!-- about -->
 			<section class="about main-border grid-row-2 grid-col-2">
 				<div class="legend">Over mij</div>
-				<p>Hallo, ik ben Brighton van Rouendal een software engineer uit nederland die gepassioneerd is in het maken van project.</p>
+				<p>Hallo, ik ben Brighton van Rouendal een software engineer uit Nederland die gepassioneerd is in het maken van project.</p>
 				<ul>
 					<p>De programmeer talen die ik ken</p>
 					<li>JavaScript</li>
@@ -45,16 +53,26 @@
 				<p>Ook lees ik graag boeken in mij vrijetijd, voornamelijk fantasy boeken. Soms start ik ook een een game op zoals satisfactory, factorio of minecraft.
 					Al ken ik nog niet veel talen ik ben altijd blij er een bij te leren.</p>
 			</section>
-			<!-- contact -->
-			<section class="main-border grid-row-2 grid-col-1">
-				<div class="legend">Contact</div>
+			<!-- Ervaring -->
+			<section class="internal main-border grid-row-2 grid-col-1">
+				<div class="legend">Ervaring</div>
+				<a href="#ervaring">Ervaringen</a>
+				<svg class="svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-link">
+					<path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"></path>
+					<path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"></path>
+				</svg>
 			</section>
 			<!-- blog -->
-			<section class="main-border grid-row-2 grid-col-1">
-				<div class="legend">Blog</div>
+			<section class="internal main-border grid-row-1 grid-col-1">
+				<div class="legend">Blog wip</div>
+				<a href="#blog">Blog</a>
+				<svg class="svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-link">
+					<path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"></path>
+					<path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"></path>
+				</svg>
 			</section>
 			<!-- projecten -->
-			<section class="projects main-border grid-row-1 grid-col-2">
+			<section class="internal main-border grid-row-1 grid-col-2">
 				<div class="legend">Projecten</div>
 				<a href="#">Projecten</a>
 				<!-- Attribute this https://freeicons.io/business-and-online-icons/link-icon-icon-2 -->
@@ -64,12 +82,14 @@
 					<path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"></path>
 				</svg>
 			</section>
+			<section class="main-border grid-row-1 grid-col-1">
+				<div class="legend">Footer</div>
+				<footer>
+					<p>© 2024 Portfolio</p>
+					<p>Homepage</p>
+				</footer>
+			</section>
 		</main>
-		<footer>
-			<div>© 2024 Portfolio</div>
-			<div class="breadcrums">Homepage</div>
-			<div><a href="mailto:Brighton@vanrouendal.nl">Email: Brighton@vanrouendal.nl</a></div>
-		</footer>
 	</div>
 </body>
 
