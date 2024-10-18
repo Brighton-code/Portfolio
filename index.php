b3499daf11112ae095d13df9a9b678fa862dc633
bright <Brighton@vanrouendal.nl>
2024-10-18 19:25:42 +0200

    changed route order

--- index.php
+++ index.php
@@ -1,13 +1,12 @@
 <?php
 
-// Make use of namespace? didn't work, kept getting not found error.
 require_once 'Core/Router.php';
 
 $router = new Router();
 
 $router->addRoute('GET', '/', ['SiteController', 'home']);
-$router->addRoute('GET', '/about', ['SiteController', 'about']);
 $router->addRoute('GET', '/:id', ['SiteController', 'homeId']);
+$router->addRoute('GET', '/about', ['SiteController', 'about']);
 $router->addRoute('GET', '/about/:id', ['SiteController', 'aboutId']);
 
 $router->matchRoute();
