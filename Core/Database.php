38d6c5201d81396303c48835d7e60a53411643fd
bright <Brighton@vanrouendal.nl>
2024-10-18 20:14:56 +0200

    database connection function

--- Core/Database.php
+++ Core/Database.php
@@ -2,5 +2,16 @@
 
 class Database {
 	public static function initialize() {
+		try {
+			$dsn = sprintf('mysql:host=%s;dbname=%s;', [$_ENV['DB_HOST'], $_ENV['DB_NAME']]);
+			$options = [
+				PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+				PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+			];
+			$conn = new PDO($dsn, $_ENV['DB_USER'], $_ENV['DB_PASS'], $options);
+			return $conn;
+		} catch (PDOException $e) {
+			var_dump($e->getMessage());
+		}
 	}
 }
