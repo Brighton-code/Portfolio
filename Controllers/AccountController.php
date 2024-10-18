6befb93ab6cbf0fbc443b2f979b488dcbf288820
bright <Brighton@vanrouendal.nl>
2024-10-18 20:46:03 +0200

    empty functions

---
+++ Controllers/AccountController.php
@@ -0,0 +1,18 @@
+<?php
+
+class AccountController extends Database {
+	public function viewLogin() {
+	}
+
+	public function login() {
+	}
+
+	public function viewRegister() {
+	}
+
+	public function register() {
+	}
+
+	public function logout() {
+	}
+}
