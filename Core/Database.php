b0c046bba92d277c42b451b3ce193f52e32ea20e
bright <Brighton@vanrouendal.nl>
2024-10-18 20:25:07 +0200

    Fixed env loading

--- Core/Database.php
+++ Core/Database.php
@@ -3,12 +3,12 @@
 class Database {
 	public static function initialize() {
 		try {
-			$dsn = sprintf('mysql:host=%s;dbname=%s;', [$_ENV['DB_HOST'], $_ENV['DB_NAME']]);
+			$dsn = sprintf('mysql:host=%s;dbname=%s;', getenv('DB_HOST'), getenv('DB_NAME'));
 			$options = [
 				PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
 				PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
 			];
-			$conn = new PDO($dsn, $_ENV['DB_USER'], $_ENV['DB_PASS'], $options);
+			$conn = new PDO($dsn, getenv('DB_USER'), getenv('DB_PASS'), $options);
 			return $conn;
 		} catch (PDOException $e) {
 			var_dump($e->getMessage());
