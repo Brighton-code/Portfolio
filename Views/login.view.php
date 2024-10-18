1ed8c3742203625d9435661d93a743d3c9a1dc3a
bright <Brighton@vanrouendal.nl>
2024-10-18 22:24:40 +0200

    login form

--- Views/login.view.php
+++ Views/login.view.php
@@ -1,4 +1,5 @@
 <?php include 'layouts/head.php'; ?>
+<link rel="stylesheet" href="/Public/css/account.css">
 
 <div class="container">
 	<header class="border">
@@ -12,7 +13,13 @@
 	</header>
 
 	<main class="whole">
-		<section class="border whole-section"></section>
+		<section class="border whole-section">
+			<form action="/login" method="post">
+				<input class="border" type="text" placeholder="Username" name="username" required maxlength="24" autocomplete="off">
+				<input class="border" type="password" placeholder="Password" name="password" required minlength="4" autocomplete="off">
+				<button class="border link" type="submit">Login</button>
+			</form>
+		</section>
 	</main>
 </div>
 
