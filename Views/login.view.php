5de0b62dccaf3a60268757870f2110974be86de1
bright <Brighton@vanrouendal.nl>
2024-10-18 21:50:23 +0200

    navbar

--- Views/login.view.php
+++ Views/login.view.php
@@ -1,5 +1,15 @@
 <?php include 'layouts/head.php'; ?>
 
-<div>Hello World</div>
+<div class="container">
+	<header class="border">
+		<nav>
+			<a class="border link" href="/">Home</a>
+			<a class="border link" href="#projects">Projects</a>
+			<a class="border link" href="#experience">Experience</a>
+			<a class="border link" href="#blog">Blog</a>
+			<a class="border link" href="#login">Login</a>
+		</nav>
+	</header>
+</div>
 
 <?php include 'layouts/foot.php'; ?>
