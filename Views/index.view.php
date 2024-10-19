63e5dfc66d843a4818c7da4edb552253e47e9c13
bright <Brighton@vanrouendal.nl>
2024-10-19 17:05:28 +0200

    start layout

---
+++ Views/index.view.php
@@ -0,0 +1,20 @@
+<?php include 'layouts/head.php';
+if (isset($error) && !empty($error)) {
+	var_dump($error);
+}
+?>
+
+<div class="container">
+	<header class="border">
+		<nav>
+			<a class="border link" href="/">Home</a>
+			<a class="border link" href="#projects">Projects</a>
+			<a class="border link" href="#experience">Experience</a>
+			<a class="border link" href="#blog">Blog</a>
+			<a class="border link" href="#login">Login</a>
+		</nav>
+	</header>
+</div>
+
+
+<?php include 'layouts/head.php' ?>
