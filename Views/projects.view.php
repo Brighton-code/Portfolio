8554f023ff5e802ec86b90b9aea5150eecee7b2c
bright <Brighton@vanrouendal.nl>
2024-10-24 22:15:46 +0200

    add internal css class back

--- Views/projects.view.php
+++ Views/projects.view.php
@@ -10,7 +10,7 @@ if (isset($error) && !empty($error)) {
 
 	<main>
 		<?php foreach ($data as $project): ?>
-			<section class="border" onclick="location.href = '/projects/<?= $project['id']; ?>'">
+			<section class="border internal" onclick="location.href = '/projects/<?= $project['id']; ?>'">
 				<h1 class="title"><?= $project['title']; ?></h1>
 				<p class="subtext"><em>Author: <?= $project['name']; ?></em></p>
 				<div class="description-container">
