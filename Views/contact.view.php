72ae107891dab1a24917cbf77132de783c890fbc
bright <Brighton@vanrouendal.nl>
2024-10-30 10:02:15 +0100

    css contacft

--- Views/contact.view.php
+++ Views/contact.view.php
@@ -8,16 +8,26 @@ if (isset($error) && !empty($error)) {
 <div class="container">
 	<?php include 'layouts/nav.php' ?>
 
-	<main>
+	<main class="contact">
+		<section class="border links">
+			<a href="mailto:brighton@vanrouendal.nl">
+				<p>My Email</p>
+			</a>
+			<a href="https://github.com/Brighton-code"><img src="/public/img/github-mark.png" alt="github" height="36"></a>
+			<a href="https://www.linkedin.com/in/brighton-van-rouendal-311448186/"><img src="/public/img/LI-In-Bug.png" alt="linkedin" height="36"></a>
+			<!-- <a href="#discord"><img src="/public/img/discord-mark-black.png" alt="discord" height="36"></a> -->
+			<a download="#cv" href="#cv">
+				<p>CV</p>
+			</a>
+		</section>
 		<section class="border">
 			<form class="save-form" action="/contact" method="post">
-				<input type="text" hidden name="post_id" value="">
-				<input type="number" hidden name="user_id" value="<?= $_SESSION['user_id']; ?>">
-				<input class="border" type="text" name="title" maxlength="50" required placeholder="Title">
-				<textarea class="border" name="description" rows="3" placeholder="Description"></textarea>
-				<textarea class="border" name="content" rows="10" required placeholder="Content"></textarea>
+				<input class="border" type="text" name="name" maxlength="50" required placeholder="Name">
+				<input class="border" type="email" name="email" placeholder="Email"></input>
+				<textarea class="border" name="message" rows="10" required placeholder="Message"></textarea>
 				<button class="border" type="submit">Submit</button>
 			</form>
+		</section>
 	</main>
 </div>
 
